b6548fcd2f87ac5d4ea792ee34d51ecc4d008c45
prayag78 <dasprayag1234@gmail.com>
2024-11-08 00:49:37 +0530

    st adding backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/',(req ,res) => {
+    res.send('Server started')
+})
+
+app.listen(port , () => console.log("Server Started at" , port))
